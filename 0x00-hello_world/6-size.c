460bacba8726a9311b93b0e5548844c9f045cc3a
triple-k <kalukalu526@gmail.com>
2022-12-08 14:17:27 -0800

    Please

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
  * main - Entry
  * Return: Always 0
  */
-int main (void)
+int main(void)
 {
 	printf("Size of a char: %i byte(s)\n", sizeof(char));
 	printf("Size of an init: %i byte(s)\n", sizeof(int));

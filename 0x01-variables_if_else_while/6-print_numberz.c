620080f8330649798b66200162a6f7c90c142d3f
triple-k <kalukalu526@gmail.com>
2022-12-09 13:12:43 -0800

    yes

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: prins all single digit number of base 10
+ * starting from 0, followed by new line.
+ * Return: 0
+ */
+int main(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}

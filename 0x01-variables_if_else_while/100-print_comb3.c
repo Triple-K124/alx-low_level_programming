649fec2091608bd7c59e27b5dfa95ee482ef72fb
triple-k <kalukalu526@gmail.com>
2022-12-09 14:58:35 -0800

    Try

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,11 +7,11 @@
  * Numbers must be separated by , followed by a space
  * The two digits must be different
  * 01 and 10 are considered the same combination of the two digits 0 and 1
- * Print only the smallest combination of two digits 
- * Numbers should be printed in ascending order, with two digits 
+ * Print only the smallest combination of two digits
+ * Numbers should be printed in ascending order, with two digits
  * You can only use the putchar function
  * (every other function (printf, puts, etc..) is forbidden)
- * You can only use putchar five times maximum in your code 
+ * You can only use putchar five times maximum in your code
  * You are not allowed to use any variable of type char
  * All your code should be in the main function
  * Return: 0

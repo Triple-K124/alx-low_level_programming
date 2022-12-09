166db7683fa442ee7a0d002073b975f8c9e370f7
triple-k <kalukalu526@gmail.com>
2022-12-09 09:48:22 -0800

    Please

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,20 +16,14 @@ int main(void)
 
 
 			srand(time(0));
-
-				n = rand() - RAND_MAX / 2;
-				if (n > 0)
-				{
+			n = rand() - RAND_MAX / 2;
+			if (n > 0)
 				printf("%d is positive\n", n);
-				}
-				else if (n == 0)
-				{
+
+			else if (n == 0)
 				printf("%d is zero\n", n);
-				}
-				(n < 0)
-				{
-					printf("%d is negative\n" n);
-				}	
-				return (0);
+			else
+				printf("%d is negative\n", n);
+			return (0);
 
 }

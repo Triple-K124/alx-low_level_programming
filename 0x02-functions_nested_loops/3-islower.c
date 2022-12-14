0da1eda9d96555d4defffe31efa1dba60f687e9a
triple-k <kalukalu526@gmail.com>
2022-12-14 05:45:12 -0800

    Task 4

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - Checks for lowercase character
- * c - The character is to be checked
+ * Description - c is the character is to be checked
  * Return: 1 for the character or 0 for anything else
  */
 

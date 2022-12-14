fe12bfe9568e6ee28cd5408d911f4342f706c065
triple-k <kalukalu526@gmail.com>
2022-12-14 05:14:33 -0800

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercase character
+ * Return: 1 for the character or 0 for anything else
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+	
+		return (1);
+	}
+	return (0);
+}

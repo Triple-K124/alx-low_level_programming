6c9ed178f36e0c191fceaef976ba6da37081c4d2
triple-k <kalukalu526@gmail.com>
2022-12-18 10:37:40 -0800

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,19 +8,19 @@
  */
 int main(void)
 {
-	int c = 0;
+	int c;
 
-	while (c < 10)
+	for (c = 0; c <= 9; c++)
 	{
-		putchar(48 + c);
+		putchar(c);
+
 		if (c != 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
-		c++;
+	putchar("\n");
 	}
 
-	putchar('\n');
 	return (0);
 }

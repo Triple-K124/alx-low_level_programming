4c4fb9be7a09f58e9e49e7db4c0755a93822d70c
triple-k <kalukalu526@gmail.com>
2022-12-19 11:46:34 -0800

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * rest_to_98 - a function that takes a pointer to an int
+ * reset_to_98 - a function that takes a pointer to an int
  * as parameter
  * and updates the value it points to 98
  * @n: input

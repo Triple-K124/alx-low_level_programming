25c8fde93f401803f69d20b6a92ba8f67a956106
triple-k <kalukalu526@gmail.com>
2022-12-19 12:51:22 -0800

    Task 3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		lens++;
+		s++;
+	}
+
+	return (len);
+}
+

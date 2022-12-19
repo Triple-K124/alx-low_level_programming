4bb03e728ae467a52f70ae394ae087add65daad1
triple-k <kalukalu526@gmail.com>
2022-12-19 09:40:42 -0800

    Task 0

---
+++ 0x05-pointers_arrays_strings/main_0.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+
+
+void reset_to_98(int *);
+
+
+
+/**
+ *
+ *  * main - check the code for Holberton School students.
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+
+int main(void)
+
+{
+
+		int n;
+
+
+
+			n = 402;
+
+				printf("%d\n", n);
+
+					reset_to_98(&n);
+
+						printf("%d\n", n);
+
+							return (0);
+
+}

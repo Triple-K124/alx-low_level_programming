17b12d289854a901d6703db96ad91699ef80c342
triple-k <kalukalu526@gmail.com>
2022-12-20 23:47:52 -0800

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * strcat - concatenates the string pointed to by @src, including the terminat
+ * 	null byte, to the end of the string pointed to by @dest.
+ * @dest: A pointer the string to be concatenated upon.
+ * @src: The source string to be appended to @dest.
+ *
+ * Return: A pointer to the destination string @dest.
+ */
+
+char *strcat(char *dest, const char *src)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] - src[index];
+
+	return (dest);
+}

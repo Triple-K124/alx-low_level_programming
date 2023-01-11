4df8a5f95b6482c5af4ece692a4c58d454bd3dbd
triple-k <kalukalu526@gmail.com>
2023-01-11 01:05:32 -0800

    Task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+/**
+ *main - program that prints the number of arguments passed into it.
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: Always 0 (success)
+ */
+
+int main(int argc, char **argv)
+
+{
+
+		int num;
+
+
+
+			for (num = 0; num < argc;)
+
+					{
+
+								num++;
+
+										(*argv)++;
+
+											}
+
+				printf("%i\n", num - 1);
+
+					return (0);
+
+}

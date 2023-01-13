5bb2853f4f477a3ceb2d3ffb7ca408374752565a
triple-k <kalukalu526@gmail.com>
2023-01-13 03:52:14 -0800

    Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ *malloc_checked - A function that allocates memory using malloc.
+ *@b: integer variable dat holds the memory size
+ *Author: Kalu Kelechi Kalu
+ *Return: 0 means program was successful
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b * sizeof(int));
+
+	if (ptr == NULL)
+	exit(98);
+	return (ptr);
+}

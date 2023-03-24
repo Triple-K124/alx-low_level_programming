fbf449e6a5c6a1b14efceb84249be23230da0942
Kalu <kalukalu526@gmail.com>
2023-03-24 08:37:53 +0000

    Task

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Compile all .c files into object files
-gcc -Wall -Werror -fpic -c *.c
+gcc -fpic -c dynamic.c -o dynamic.o
 
 # Create the dynamic library
-gcc -shared -o liball.so *.o
+gcc -shared -o liball.so dynamic.o
 
 # Cleanup: remove the object files
 rm *.o
+
+sudo ldconfig

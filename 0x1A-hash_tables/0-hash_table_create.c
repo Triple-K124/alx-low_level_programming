c31485c5290a61c891bb07b64edd6a2b4dac6c84
Kalu <kalukalu526@gmail.com>
2023-04-07 15:01:35 +0000

    Task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,8 +13,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned int i;
 	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_node_t *));
 
+	if (!table)
+		return (NULL);
 	table->size = size;
 	table->array = calloc(table->size, sizeof(hash_node_t *));
+		if (!table->array)
+			return (NULL);
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
 	return (table);

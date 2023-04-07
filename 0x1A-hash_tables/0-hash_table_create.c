b81ddec64f508b54f11ee82b33e3c6fccbd59d72
Kalu <kalukalu526@gmail.com>
2023-04-07 16:07:56 +0000

    Task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "hash_tables.h"
+
 /**
  * hash_table_create - creates a new hash table
  * @size: size of the hash table
@@ -11,7 +12,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned int i;
-	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_node_t *));
+	hash_table_t *table =(hash_table_t *)malloc(sizeof(hash_table_t));
 
 	if (!table)
 	{
@@ -19,13 +20,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	table->size = size;
-	table->array = calloc(table->size, sizeof(hash_node_t *) * size);
+	table->array = calloc(table->size, sizeof(hash_node_t *));
 		if (!table->array)
 		{
-			fprintf(stderr, "Error: malloc failed\n");
+			fprintf(stderr, "Error: calloc failed\n");
 			return (NULL);
 		}
-	for (i = 0; i < table->size; i++)
+	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
 	return (table);
 }

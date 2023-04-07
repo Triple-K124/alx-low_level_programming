c00982ac28e32502e6d2d3977c9b1c901fc9abbe
Kalu <kalukalu526@gmail.com>
2023-04-07 15:11:59 +0000

    Task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,8 +18,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->size = size;
 	table->array = calloc(table->size, sizeof(hash_node_t *));
 		if (!table->array)
+		{
+			free(table);
 			return (NULL);
+		}
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
+	
 	return (table);
 }

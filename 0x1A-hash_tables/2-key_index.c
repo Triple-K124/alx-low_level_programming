7ae8bed9498111e0d2f903f0e4f8a7de2229c2b1
Kalu <kalukalu526@gmail.com>
2023-04-09 19:48:38 +0000

    Task

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+/**
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
+ */
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index_key = hash_djb2(key) % size;
+	return (index_key);
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -31,5 +31,8 @@ typedef struct hash_table_s
 } hash_table_t;
 unsigned long int hash_djb2(const unsigned char *str);
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+
+
 
 #endif

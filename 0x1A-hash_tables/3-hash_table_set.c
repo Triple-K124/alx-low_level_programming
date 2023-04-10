6921677afe126f67ce9143b97ab74ccd076ca1ba
Kalu <kalukalu526@gmail.com>
2023-04-10 13:07:48 +0000

    Task

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -6,11 +6,11 @@
 
 /**
  * key_exists - checks if the key really exists.
- * @ht - stores the hash table.
- * @key - the key of the string.
- * @value - the value that will be stored in the string
- * k_index - stores the hash value
- * check - checks if the key is already there.
+ * @ht: stores the hash table.
+ * @key: the key of the string.
+ * @value: the value that will be stored in the string
+ *
+ * Return: Returns 0 if successful
  */
 
 int key_exists(hash_table_t *ht, const char *key, const char *value)
@@ -35,13 +35,11 @@ int key_exists(hash_table_t *ht, const char *key, const char *value)
 }
 /**
  * update_value - updates the value of the key.
- * @ht - stores the hash table.
- * @key - the key of the string.
- * @value - the value that will be stored in the string
- * k_index - stores the hash value
- * check - checks if the key is already there.
+ * @ht: stores the hash table.
+ * @key: the key of the string.
+ * @value: the value that will be stored in the string
  *
- * return - Returns 0 if successful
+ * Return: Returns 0 if successful
  */
 
 int update_value(hash_table_t *ht, const char *key, const char *value)
@@ -64,10 +62,10 @@ int update_value(hash_table_t *ht, const char *key, const char *value)
 
 /**
  * create_node - creates new node.
- * @key - the key of the string.
- * @value - the value that will be stored in the string
+ * @key: the key of the string.
+ * @value: the value that will be stored in the string
  *
- * Return - Return 0 if successful 
+ * Return: Return 0 if successful
  */
 
 hash_node_t *create_node(const char *key, const char *value)
@@ -78,19 +76,28 @@ hash_node_t *create_node(const char *key, const char *value)
 	if (!new_node)
 	{
 		fprintf(stderr, "New memory was not allocated");
-		return NULL;
+		return (NULL);
 	}
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 	new_node->next = NULL;
-	return new_node;
+	return (new_node);
 }
+/**
+ * hash_table_set - Addition of the key into the table
+ * @ht: This is the table itself
+ * @key: The key that would be added
+ * @value: The value that is tied with the key
+ *
+ * Return: The new table that has been added a new new
+ */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	
+
 	hash_node_t *new_node;
 	unsigned long int k_index;
+
 	if (!ht || !key)
 	{
 		fprintf(stderr, "Hash table or key doesn't exist");

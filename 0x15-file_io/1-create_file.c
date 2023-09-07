5b2ab50aa465780f70d8ebb31dfe58f4a8a7dad2
Kalu <kalukalu526@gmail.com>
2023-09-07 10:06:40 +0000

    Task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,12 +4,13 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/stat.h>
+
 /**
- * create_file - A function that creates a file
- * @filename - The file that is being created
- * @text_content - Text that will be written into the file
+ * create_file - Creates a file.
+ * @filename: The name of the file to create or truncate
+ * @text_content: The text content to write into the file (can be NULL).
  *
- * Return - Return 1 on success, -1 on failure.
+ * Return: Return 1 on success, -1 on failure.
  */
 
 int create_file(const char *filename, char *text_content)
@@ -19,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	ssize_t writer;
 
 	if (filename == NULL)
-		return -1;
+		return (-1);
 	fd = open(filename, O_CREAT | O_TRUNC | S_IRUSR | S_IWUSR);
 
 	if (fd == -1)
@@ -50,5 +51,4 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 
 		return (1);
-	
 }

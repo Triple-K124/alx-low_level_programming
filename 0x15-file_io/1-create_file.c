32214b08ba272254e4017f47df2313d7c049a735
Kalu <kalukalu526@gmail.com>
2023-11-28 11:12:32 +0000

    Task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,16 +22,16 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	
+
 	if (text_content == NULL)
 		text_content = "";
-	
-	 if (text_content != NULL)
+
+	if (text_content != NULL)
 	{
-		 for (i = 0; text_content[i] != '\0'; i++)
-		 {
-		 	len_text++;
-		 }
+		for (i = 0; text_content[i] != '\0'; i++)
+		{
+			len_text++;
+		}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC | 0600);
 

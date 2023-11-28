d18ea3765e00caa350d3f714a73cef24019a1fca
Kalu <kalukalu526@gmail.com>
2023-11-28 05:57:59 +0000

    Task

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,6 +6,13 @@
 #include <sys/stat.h>
 
 #define BUFFER_SIZE 1024
+/**
+* print_error - Prints the error to standard output
+* @code: The error code
+* @message: The error message to be displayed
+* @arg: Points to the string
+* return: Return nothing
+*/
 
 void print_error(int code, const char *message, const char *arg)
 {
@@ -13,10 +20,16 @@ void print_error(int code, const char *message, const char *arg)
 	exit(code);
 }
 
+/**
+* main - Entry point of the code
+* @argc: The argument count of the function
+* @argv: The arguments
+*
+* Return: Return 1 if successsful
+*/
 int main(int argc, char *argv[])
 {
-	const char *file_from = argv[1];
-	const char *file_to = argv[2];
+	const char *file_from = argv[1], *file_to = argv[2];
 	int fd_from, fd_to;
 	char buffer[BUFFER_SIZE];
 	ssize_t bytes_read, bytes_written;
@@ -24,20 +37,17 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		print_error(97, "Usage: %s file_from file_to\n", argv[0]);
-    	}
-
+	}
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
 	{
 		print_error(98, "Error: Can't read from file %s\n", file_from);
 	}
-
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		print_error(99, "Error: Can't write to %s\n", file_to);
 	}
-
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
@@ -46,21 +56,17 @@ int main(int argc, char *argv[])
 			print_error(99, "Error: Can't write to %s\n", file_to);
 		}
 	}
-
 	if (bytes_read == -1)
 	{
 		print_error(98, "Error: Can't read from file %s\n", file_from);
 	}
-
 	if (close(fd_from) == -1)
 	{
 		print_error(100, "Error: Can't close fd %d\n", argv[1]);
 	}
-
 	if (close(fd_to) == -1)
 	{
 		print_error(100, "Error: Can't close fd %d\n", argv[2]);
-    	}
-
+	}
 	return (0);
 }
